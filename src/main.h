76399eb24c45d2d4b82fc6abfa70f78f250a8e46
Oliver Brandmueller <ob@silentium.fritz.box>
2018-11-08 19:28:19 +0100

    start splitting of functions

---
+++ src/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <Arduino.h>
+#include "WiFi.h"
+#endif
---
+++ src/translateEncryptionType.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+
+String translateEncryptionType(wifi_auth_mode_t encryptionType) {
+  switch (encryptionType) {
+    case (WIFI_AUTH_OPEN):
+      return "Open";
+    case (WIFI_AUTH_WEP):
+      return "WEP";
+    case (WIFI_AUTH_WPA_PSK):
+      return "WPA_PSK";
+    case (WIFI_AUTH_WPA2_PSK):
+      return "WPA2_PSK";
+    case (WIFI_AUTH_WPA_WPA2_PSK):
+      return "WPA_WPA2_PSK";
+    case (WIFI_AUTH_WPA2_ENTERPRISE):
+      return "WPA2_ENTERPRISE";
+    case (WIFI_AUTH_MAX):
+      return "MAX";
+    default:
+      return "Unknown";
+  }
+}
